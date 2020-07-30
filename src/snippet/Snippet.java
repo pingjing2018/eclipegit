b0b6c76394bc446f08ac38a52df8c4c2f9fb2e5a
zyy <459478315@qq.com>
2020-07-30 11:40:35 +0800

    fff

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -3,7 +3,7 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		System.out.print("dddddd11");
-		System.out.print("dddddd1122");
+		System.out.print("dddddd112255555");
 		
 	}
 }
